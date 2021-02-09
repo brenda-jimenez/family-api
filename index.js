590160aac481e6067a183db89dbf846f839bc335
Brenda Jimenez <brenda.jimenez@levvel.io>
2021-02-08 19:14:19 -0600

    setting host and port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
+const host= process.env.HOST
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, host, () => {
+  console.log(`Example app listening at http://${host}:${port}`)
 })
